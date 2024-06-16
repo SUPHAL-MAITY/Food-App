a569038f84bec98eeaa7e269f6c54aad5ca59a08
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-06-16 07:33:21 +0530

    bug fix in update of restaurant

--- frontend/src/Pages/Restaurant.jsx
+++ frontend/src/Pages/Restaurant.jsx
@@ -20,7 +20,7 @@ const Restaurant = () => {
    useEffect(()=>{
     getRestaurants()
 
-   },[pickup])
+   },[restaurants])
 
     const getRestaurants=async()=>{
         try {
@@ -284,22 +284,7 @@ const Restaurant = () => {
 
 
                 </div>
-                {/* <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Submit
-                  </button>
-                </div> */}
+                
               </div>
             </div>
           </div>
