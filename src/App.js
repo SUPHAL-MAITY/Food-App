fc276088b7d4700942ef0c5a4d31653f0597a736
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-09-19 16:56:09 +0530

    change for deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors({
     // origin: process.env.CORS_ORIGIN,
-    origin: "'https://food-app-delta-ecru.vercel.app'",
+    origin: "https://food-app-delta-ecru.vercel.app",
     
     credentials: true
 }))
