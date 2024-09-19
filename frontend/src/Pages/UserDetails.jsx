c498e20c98ca7ca09362cf3067df26e886b72542
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-09-19 18:51:01 +0530

    navbar bug -by transparent

--- frontend/src/Pages/UserDetails.jsx
+++ frontend/src/Pages/UserDetails.jsx
@@ -86,8 +86,8 @@ const handleDelete=async(id)=>{
 
 
   return (
-<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">       
-<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-transparent py-6 sm:py-12">       
+<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-transparent py-6 sm:py-12">
   <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
     <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]" />
     <div className="relative z-10 mx-auto max-w-md">
